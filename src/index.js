f8b0732deb9560d91350ced1f6d891bc905df2d9
hanhaihuoc <74859369+hanhaihuoc@users.noreply.github.com>
2022-09-18 10:30:22 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 const Discord = require("discord.js");
-
 const client = new Discord.Client();
 
 const config = {
     prefix: "-",
-    token: "token"
-}
+    token: "" }
 
 client.on("ready", () => {
-    console.log("Đập Đá Thôi Nào!")
-})
+    console.log("Đập Đá Thôi Nào!") })
 
 client.on("message", (message) => {
 
-    client.user.setPresence({ activity: { name: 'Đập Đá' }, status: 'dnd' }) 
+client.user.setPresence({ activity: { name: 'Đập Đá' }, status: 'dnd' }) 
 
     if(message.author.bot) return;
     if(!message.content.startsWith(config.prefix)) return;
@@ -21,59 +18,81 @@ client.on("message", (message) => {
     const args = message.content.slice(config.prefix.length).split(" ");
     const command = args.shift();    
     
-  switch(command){
+    switch(command){
 
-  // Random Chào Hỏi [001]
-  case "hello":      
+// --- Một Số Trả Về Random Theo Điều Kiện Được Thiết Lập Trước [001] 
+
+case "hello":      
     var hello = Math.floor(Math.random() * 4);
-    if (hello === 0) {
-          message.channel.send("hello bạn <a:c_woww:826123141092474891>"); }
+    if      (hello === 0) {
+          message.channel.send("hello [001]"); }
     else if (hello === 1) {
-          message.channel.send("hello bạn xinh đẹp");  }
+          message.channel.send("hello [002]"); }
     else if (hello === 2) {
-          message.channel.send("Knichiwa");  }
-    else {
-          message.channel.send("chào mày <:P_xienmaygio:826136559832399882>"); }   
+          message.channel.send("hello [003]"); }
+    else if (hello === 3) {
+          message.channel.send("hello [004]"); }
+    else    {
+          message.channel.send("hello [005]"); }   
+    break;  
 
+case "sad":      
+    var sad = Math.floor(Math.random() * 4);
+    if      (sad === 0) {
+          message.channel.send("sad [001]"); }
+    else if (sad === 1) {
+          message.channel.send("sad [002]"); }
+    else if (sad === 2) {
+          message.channel.send("sad [003]"); }
+    else if (sad === 3) {
+          message.channel.send("sad [004]"); }
+    else    {
+          message.channel.send("sad [005]"); }   
+    break; 
+
+case "fun":      
+    var fun = Math.floor(Math.random() * 4);
+    if      (fun === 0) {
+          message.channel.send("fun [001]"); }
+    else if (fun === 1) {
+          message.channel.send("fun [002]"); }
+    else if (fun === 2) {
+          message.channel.send("fun [003]"); }
+    else if (fun === 3) {
+          message.channel.send("fun [004]"); }
+    else    {
+          message.channel.send("fun [005]"); }   
+    break; 
+
+// --- Kiểm Tra Độ Trễ Của Mạng [002]
+
+case "ping":         
+         message.channel.send(`Độ trễ mạng của bạn là: ${client.ws.ping}`);
     break;
 
-    case "sad":      
-    var sad = Math.floor(Math.random() * 2);
-    if (sad === 0) {
-         message.channel.send("<:P1_omnhaukhoc:852895249612144661> Đừng buồn nữa, hãy vui lên nhé bạn ơi"); }
-    else {
-        message.channel.send("<:P1_omnhaukhoc:852895249612144661> Mọi chuyện sẽ ổn thôi, có tớ ở đây với cậu mà "); } 
-    break;
+// --- Xem Avatar Của Bản Thân Và Người Khác [003]
 
-    case "fun":      
-    var fun = Math.floor(Math.random() * 2);
-    if (fun === 0) {
-         message.channel.send("<:B_lmao:853162483576340501> Dảk Dảk Bủh Bủh Lmao Lmao"); }
-    else {
-        message.channel.send(" <:B_lmao:853162483576340501> Jajaja kkkkk LOL Lmao Lmao"); } 
-    break;
-
-  // Kiểm Tra Tốc Độ Mạng [002]
-    case "ping":         
-         message.channel.send(`<:P2_hinhlamay:852892547129802776> Tốc độ mạng của bạn là: ${client.ws.ping}`);
-    break;
-
-  // Xem Avatar [003]
-    case "av":         
+case "av":         
     if(message.mentions.users.size){
          let member=message.mentions.users.first()
     if(member){
-         const emb=new Discord.MessageEmbed().setImage(member.displayAvatarURL()).setTitle(member.tag)
+         const emb=new Discord.MessageEmbed()
+         .setImage(member.displayAvatarURL())
+         .setTitle(member.tag)
          message.channel.send(emb)}
     else {
-         message.channel.send("Xin lỗi nhưng tôi không tìm được người này, bạn có thể giúp tôi kiểm tra lại được chứ ")} }
+         message.channel.send("không tìm thấy")} }
     else {
-      const emb=new Discord.MessageEmbed().setImage(message.author.displayAvatarURL()).setTitle(message.author.tag)
+      const emb=new Discord.MessageEmbed()
+          .setImage(message.author
+          .displayAvatarURL())
+          .setTitle(message.author.tag)
          message.channel.send(emb) }
     break;
 
-  // Random Emoji [004]
-    case "emoji":
+// --- Random Emoji [004]
+
+case "emoji":
     var emoji = Math.floor(Math.random() * 2);
     if (emoji === 0) {
          message.channel.send(":)"); }
@@ -81,27 +100,27 @@ client.on("message", (message) => {
          message.channel.send("=)"); }
     break;
 
-  // Random Number [005]
-    case "random":
-        message.channel.send("<:D_yes:828800864344342568> Đang sổ số. Con số may mắn của bạn là: ||" + Math.floor(Math.random() * 1001 ) + "||");
+// --- Random Number [005]
+
+case "random":
+         message.channel.send("Số may mắn của bạn là: " + Math.floor(Math.random() * 1001 ));
     break;
-    case "say":
+
+// --- Phản Hồi Khi Người Dùng Nhập Sai Lệnh hoặc Rỗng [006]
+
+case "say":
          message.channel.send(args.join(" "));
     break;
-  // Phản Hồi Khi Người Dùng Nhập Sai Lệnh [006]
     default:
-         message.channel.send("Tôi không thể hiểu bạn nói gì <:A_lasao:826683507509821490>, ``-help`` để biết thêm");
+         message.channel.send("Tôi không thể hiểu bạn nói gì <:A_lasao:826683507509821490>");
     break;
-
-  // Dự Báo Thời Tiết []
-
+    
   // Phiên Dịch Ngôn Ngữ []
 
   // Chơi Nhạc []
 
   // Tạo Give-Away []
 
-  // 
   }
 
 })
